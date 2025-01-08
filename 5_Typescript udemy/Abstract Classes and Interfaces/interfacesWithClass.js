1ec38a3013f4b1c2099bbcde0524146de3c76ef7
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-08 20:32:16 +0545

    finish: abstract classes and interfaces in typescript

--- 5_Typescript udemy/Abstract Classes and Interfaces/interfacesWithClass.js
+++ 5_Typescript udemy/Abstract Classes and Interfaces/interfacesWithClass.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var module1;
 (function (module1) {
     var AutomobileType;
@@ -36,3 +51,105 @@ var module1;
     var gadi = new car(AutomobileBrand.ferrari, [AutomobileColors.black, AutomobileColors.blue]);
     console.log(gadi);
 })(module1 || (module1 = {}));
+//? now implementing use of multiple interfaces in classes
+var module2;
+(function (module2) {
+    var AutomobileType;
+    (function (AutomobileType) {
+        AutomobileType["car"] = "car";
+        AutomobileType["bus"] = "bus";
+        AutomobileType["truck"] = "truck";
+        AutomobileType["bike"] = "bike";
+    })(AutomobileType || (AutomobileType = {}));
+    var AutomobileBrand;
+    (function (AutomobileBrand) {
+        AutomobileBrand["ferrari"] = "ferrari";
+        AutomobileBrand["proche"] = "proche";
+        AutomobileBrand["bmw"] = "BMW";
+        AutomobileBrand["honda"] = "honda";
+        AutomobileBrand["toyota"] = "toyota";
+    })(AutomobileBrand || (AutomobileBrand = {}));
+    var AutomobileColors;
+    (function (AutomobileColors) {
+        AutomobileColors["green"] = "green";
+        AutomobileColors["pink"] = "pink";
+        AutomobileColors["blue"] = "blue";
+        AutomobileColors["yellow"] = "Yellow";
+        AutomobileColors["black"] = "black";
+        AutomobileColors["silver"] = "silver";
+    })(AutomobileColors || (AutomobileColors = {}));
+    var truck = /** @class */ (function () {
+        function truck(brand, color) {
+            this.brand = brand;
+            this.color = color;
+            this.name = "truck";
+            this.description = "this is a new truck";
+            this.capacity = "five";
+            this.lisenceRenewDate = new Date();
+        }
+        return truck;
+    }());
+    var newTruck = new truck(AutomobileBrand.proche, [AutomobileColors.pink]);
+    console.log(newTruck);
+})(module2 || (module2 = {}));
+var users = /** @class */ (function () {
+    function users(name, id, hashPassword) {
+        this.name = name;
+        this.id = id;
+        this.hashPassword = hashPassword;
+    }
+    return users;
+}());
+//? this is not possible in interfaces
+//? another example is we cannot redeclear same type name but in interfaces we can
+//TODO:difference between abstract class and interfaces
+//? here both provides the binding contract ...like properties or method defined by the user should be present there
+//? in abstract class it can have function implementation while in interface only call signature is possible
+//? in interface we can implement multiple interface whereas in abstract classes we cannot implement that
+//? in abstract we can implement static method but in interface we cannot.
+var abst;
+(function (abst) {
+    var Person = /** @class */ (function () {
+        function Person() {
+        }
+        Person.prototype.greeting = function () {
+            console.log("hello ".concat(this.name));
+        };
+        Person.nameClass = function () {
+            return 'this is a Parent class Person';
+        };
+        return Person;
+    }());
+    var registerPerson = /** @class */ (function (_super) {
+        __extends(registerPerson, _super);
+        function registerPerson(name, email, phone) {
+            var _this = _super.call(this) || this;
+            _this.name = name;
+            _this.email = email;
+            _this.phone = phone;
+            return _this;
+        }
+        return registerPerson;
+    }(Person));
+    var person = new registerPerson('prajwol', 'sheresth@gmail.com', 8764728293);
+    person.greeting();
+    console.log(Person.nameClass());
+    console.log(person);
+})(abst || (abst = {}));
+var inter;
+(function (inter) {
+    var registerPerson = /** @class */ (function () {
+        function registerPerson(name, email, phone) {
+            this.name = name;
+            this.email = email;
+            this.phone = phone;
+        }
+        registerPerson.prototype.greeting = function () {
+            console.log("hello ".concat(this.name));
+        };
+        return registerPerson;
+    }());
+    var person = new registerPerson('prajowl', 'shrestha@gmail.com', 9867343768);
+    person.greeting();
+    console.log(person);
+})(inter || (inter = {}));
--- 5_Typescript udemy/Abstract Classes and Interfaces/interfacesWithClass.ts
+++ 5_Typescript udemy/Abstract Classes and Interfaces/interfacesWithClass.ts
@@ -94,3 +94,85 @@ namespace module2{
     console.log(newTruck)
 }
 
+//?how access modifier works in conjuction with the interfaces
+//! here one thing to rememeber is that when you make the interface then it only works for public ...which means in class we cannot will be able to make properties or method private that are written in interface
+
+
+//? interface merging - what is say's is like when we declear the interface multiple times with same name typeScript merge it and treat it as same single interface....below is the code
+
+interface Iuser{
+    name:string,
+    id:number
+}
+
+interface Iuser{
+    hashPassword:string;
+}
+
+class users implements Iuser{
+    constructor(public name:string,public id :number,public hashPassword:string){
+
+    }
+}
+
+//TODO: difference between type alias and interfaces:- using type alias we can use intersection i.e. & and union i.e. | .... but interfaces cannot do it ....interfaces can extends with another interfaces 
+
+//?another is we can define tuples inside type alias where as in interfaces we cannot
+type TupleExample=[string,number];
+//? this is not possible in interfaces
+//? another example is we cannot redeclear same type name but in interfaces we can
+
+//TODO:difference between abstract class and interfaces
+//? here both provides the binding contract ...like properties or method defined by the user should be present there
+//? in abstract class it can have function implementation while in interface only call signature is possible
+//? in interface we can implement multiple interface whereas in abstract classes we cannot implement that
+//? in abstract we can implement static method but in interface we cannot.
+
+namespace abst{
+    abstract class Person{
+        public abstract name:string;
+        public abstract email:string;
+        public abstract phone:number;
+
+        public greeting(){
+            console.log(`hello ${this.name}`)
+        }
+        public static nameClass(){
+            return 'this is a Parent class Person'
+        }
+    }
+    class registerPerson extends Person{
+      public constructor(public name:string,public email:string,public phone:number){
+        super()
+       }
+    }
+    const person:registerPerson=new registerPerson('prajwol','sheresth@gmail.com',8764728293)
+    person.greeting()
+    console.log(Person.nameClass())
+    console.log(person)
+}
+namespace inter{
+    interface Person{
+        name:string,
+        email:string,
+        phone:number
+        // greeting():void//? we can only have call signatures
+    }
+    interface greet{
+        greeting:()=>void;
+    }
+    class registerPerson implements Person,greet{
+     constructor(public name:string, public email:string, public phone:number){
+     }
+     greeting(){
+        console.log(`hello ${this.name}`)
+     }
+    }
+    const person:registerPerson=new registerPerson('prajowl','shrestha@gmail.com',9867343768)
+    person.greeting()
+    console.log(person)
+}
+
+    
+
+//? so which one to choose ..if you have to make the methods and static methods then you should consider using abstract class but if you want only to use call signature and you want to bound the class to follow and use certain properties then you should consider using interfaces....BUT the best choices to see the stack overflow and see what other people have used to solve that particular problem that you are looking for.
