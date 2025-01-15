515ff39c185496f6b57a8fea3e687438588b718c
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-15 16:13:29 +0545

    wip:- truthiness Narrowing in typeScript

--- 5_Typescript udemy/Type Guards in TypeScript/typeOfTypeGuard.ts
+++ 5_Typescript udemy/Type Guards in TypeScript/typeOfTypeGuard.ts
@@ -15,3 +15,27 @@
     //? or we can do in this case is this
 
     //? if(str && typeof str=='object') //now it's only runs when it's not null
+
+
+//! not let's learn about truthiness Narrowing
+
+//? falsy value are Null, undefined, false, 0, NAN, ''
+
+//? it is the process of refining a type from variable based on the condition that checks if it is truthy or falsy.
+type Person={
+    name:string;
+    age?:number;
+};
+
+function printAge(person:Person){
+    if(person.age){
+        console.log(person.age);
+    }else{
+        console.log("Age Unknown");
+    }
+}
+
+printAge({
+    age:"john",//? here it says this should be number becaue we have done truthiness narrowing 
+    name:"john"
+})
