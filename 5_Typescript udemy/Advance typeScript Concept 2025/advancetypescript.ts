ee3fde216180ae31b0ff1056e90b05cff6d7e2bc
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-06-04 16:27:37 +0545

    wip: type widening and type narrowing

--- 5_Typescript udemy/Advance typeScript Concept 2025/advancetypescript.ts
+++ 5_Typescript udemy/Advance typeScript Concept 2025/advancetypescript.ts
@@ -62,3 +62,35 @@ const dog:Dog={
 
 const animal:Animal=dog// here it will not complain because dog is subtype of Animal
 
+
+const newAnimal1=function (data:Animal){
+    console.log(data.name)
+}
+
+const newAnimal:(animal:Dog)=>void=newAnimal1
+
+
+newAnimal({name:"husky",barks:true});
+
+//this is incase of function paramter asking for Dog type but we put Animal type 
+
+
+
+//type Narrowing and type widening
+
+const test1="hello" // "hello" literal value 
+
+let test2="hi" //string
+
+//this is type narrowing since test2 is of let so it can be andy value of type string as it can be reassigned but not in constant (test1)
+
+//type narrowing
+
+let test3:string |undefined;
+
+// test3.length// it will give error since test3 can be undefined as well
+
+if(test3){
+    console.log(test3.length) // now we narrow down to the string
+}
+
